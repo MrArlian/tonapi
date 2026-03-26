01d79fdd1b14b021073d0454ae64edce87bfef46
ness <shonness98@gmail.com>
2026-03-27 00:35:50 +0500

    Cache resource instances in mixin

--- pytonapi/rest/mixin.py
+++ pytonapi/rest/mixin.py
@@ -33,98 +33,22 @@ class ResourcesMixin:
 
     def __init__(self, client: TonapiRestClient) -> None:
         self._client = client
-
-    @property
-    def accounts(self) -> AccountsResource:
-        """Access :class:`AccountsResource` resource group."""
-        return AccountsResource(self._client)
-
-    @property
-    def blockchain(self) -> BlockchainResource:
-        """Access :class:`BlockchainResource` resource group."""
-        return BlockchainResource(self._client)
-
-    @property
-    def connect(self) -> ConnectResource:
-        """Access :class:`ConnectResource` resource group."""
-        return ConnectResource(self._client)
-
-    @property
-    def dns(self) -> DNSResource:
-        """Access :class:`DNSResource` resource group."""
-        return DNSResource(self._client)
-
-    @property
-    def emulation(self) -> EmulationResource:
-        """Access :class:`EmulationResource` resource group."""
-        return EmulationResource(self._client)
-
-    @property
-    def events(self) -> EventsResource:
-        """Access :class:`EventsResource` resource group."""
-        return EventsResource(self._client)
-
-    @property
-    def extra_currency(self) -> ExtraCurrencyResource:
-        """Access :class:`ExtraCurrencyResource` resource group."""
-        return ExtraCurrencyResource(self._client)
-
-    @property
-    def gasless(self) -> GaslessResource:
-        """Access :class:`GaslessResource` resource group."""
-        return GaslessResource(self._client)
-
-    @property
-    def jettons(self) -> JettonsResource:
-        """Access :class:`JettonsResource` resource group."""
-        return JettonsResource(self._client)
-
-    @property
-    def lite_server(self) -> LiteServerResource:
-        """Access :class:`LiteServerResource` resource group."""
-        return LiteServerResource(self._client)
-
-    @property
-    def multisig(self) -> MultisigResource:
-        """Access :class:`MultisigResource` resource group."""
-        return MultisigResource(self._client)
-
-    @property
-    def nft(self) -> NFTResource:
-        """Access :class:`NFTResource` resource group."""
-        return NFTResource(self._client)
-
-    @property
-    def purchases(self) -> PurchasesResource:
-        """Access :class:`PurchasesResource` resource group."""
-        return PurchasesResource(self._client)
-
-    @property
-    def rates(self) -> RatesResource:
-        """Access :class:`RatesResource` resource group."""
-        return RatesResource(self._client)
-
-    @property
-    def staking(self) -> StakingResource:
-        """Access :class:`StakingResource` resource group."""
-        return StakingResource(self._client)
-
-    @property
-    def storage(self) -> StorageResource:
-        """Access :class:`StorageResource` resource group."""
-        return StorageResource(self._client)
-
-    @property
-    def traces(self) -> TracesResource:
-        """Access :class:`TracesResource` resource group."""
-        return TracesResource(self._client)
-
-    @property
-    def utilities(self) -> UtilitiesResource:
-        """Access :class:`UtilitiesResource` resource group."""
-        return UtilitiesResource(self._client)
-
-    @property
-    def wallet(self) -> WalletResource:
-        """Access :class:`WalletResource` resource group."""
-        return WalletResource(self._client)
+        self.accounts = AccountsResource(client)
+        self.blockchain = BlockchainResource(client)
+        self.connect = ConnectResource(client)
+        self.dns = DNSResource(client)
+        self.emulation = EmulationResource(client)
+        self.events = EventsResource(client)
+        self.extra_currency = ExtraCurrencyResource(client)
+        self.gasless = GaslessResource(client)
+        self.jettons = JettonsResource(client)
+        self.lite_server = LiteServerResource(client)
+        self.multisig = MultisigResource(client)
+        self.nft = NFTResource(client)
+        self.purchases = PurchasesResource(client)
+        self.rates = RatesResource(client)
+        self.staking = StakingResource(client)
+        self.storage = StorageResource(client)
+        self.traces = TracesResource(client)
+        self.utilities = UtilitiesResource(client)
+        self.wallet = WalletResource(client)
